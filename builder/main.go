cdf216d231622c0d923124301d58238ee5c74e98
tamama9527 <deus85072@mail.fcu.edu.tw>
2021-03-08 23:12:06 +0800

    test3

--- builder/main.go
+++ builder/main.go
@@ -16,14 +16,8 @@ func main() {
     util.NotionExport(client, "03970d328a2f4278859915d0a88ae830")
     util.NotionPage(client, "03970d328a2f4278859915d0a88ae830")
     os.Chdir("../")
-    cmd := &exec.Cmd {
-        Path: "./gitpush.sh",
-        Args: []string{"gitpush.sh", "test shell script"},
-        Stdout: os.Stdout,
-        Stderr: os.Stdout,
-        Dir : "~/tamama_blog/",
-    }
-    if err := cmd.Run; err != nil {
+    cmd := exec.Command("./gitpush.sh","test3")
+    if err := cmd.Run(); err != nil {
         fmt.Println("Error:", cmd.String());
         fmt.Println("Error:", cmd.Stderr);
     }

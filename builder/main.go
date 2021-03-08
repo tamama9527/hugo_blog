18d4a5e792ad1d4f0c2fb27a07b99e9618f254c6
tamama9527 <deus85072@mail.fcu.edu.tw>
2021-03-08 22:38:47 +0800

    test

--- builder/main.go
+++ builder/main.go
@@ -15,14 +15,17 @@ var client = util.NotionClient(os.Getenv("NOTION_AUTH_token"))
 func main() {
     util.NotionExport(client, "03970d328a2f4278859915d0a88ae830")
     util.NotionPage(client, "03970d328a2f4278859915d0a88ae830")
+    os.Chdir("../")
     cmd := &exec.Cmd {
-        Path: "../gitpush.sh",
+        Path: "./gitpush.sh",
         Args: []string{"gitpush.sh", "test shell script"},
         Stdout: os.Stdout,
         Stderr: os.Stdout,
+        Dir : "~/tamama_blog/",
     }
     if err := cmd.Run; err != nil {
         fmt.Println("Error:", cmd.String());
+        fmt.Println("Error:", cmd.Stderr);
     }
 
 }
